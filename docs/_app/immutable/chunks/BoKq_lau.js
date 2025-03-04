bfad3ce2ff1b1ab92ba6492f79d89186564f375a
Milan Schollier <milan.schollier@gmail.com>
2025-03-04 09:52:09 +0100

    :hammer: static site voor location toegevoegd

---
+++ docs/_app/immutable/chunks/BoKq_lau.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_193g30z)==null?void 0:s.base)??"/GPS_obfuscator_android_module";var a;const t=((a=globalThis.__sveltekit_193g30z)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ docs/_app/immutable/entry/start.BH1bKg14.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/Or6rwc5K.js";export{t as start};
